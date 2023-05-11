5b38ea07170a200c93bac79033f8625e0952ba67
markowar <kodpromocyjny1@gmail.com>
2023-05-11 15:25:23 +0200

    obrot

--- obraz_wizualne/Form1.Designer.cs
+++ obraz_wizualne/Form1.Designer.cs
@@ -30,6 +30,9 @@
         {
             pictureBox1 = new PictureBox();
             Wczytaj = new Button();
+            obrot_90 = new Button();
+            pion = new Button();
+            poziom = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -37,7 +40,7 @@
             // 
             pictureBox1.Location = new Point(12, 12);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(393, 285);
+            pictureBox1.Size = new Size(530, 312);
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             pictureBox1.Click += pictureBox1_Click;
@@ -52,11 +55,44 @@
             Wczytaj.UseVisualStyleBackColor = true;
             Wczytaj.Click += Wczytaj_Click;
             // 
+            // obrot_90
+            // 
+            obrot_90.Location = new Point(582, 246);
+            obrot_90.Name = "obrot_90";
+            obrot_90.Size = new Size(173, 51);
+            obrot_90.TabIndex = 2;
+            obrot_90.Text = "Obrót 90st.";
+            obrot_90.UseVisualStyleBackColor = true;
+            obrot_90.Click += obrot_90_Click;
+            // 
+            // pion
+            // 
+            pion.Location = new Point(582, 303);
+            pion.Name = "pion";
+            pion.Size = new Size(173, 52);
+            pion.TabIndex = 3;
+            pion.Text = "Odbicie w pionie";
+            pion.UseVisualStyleBackColor = true;
+            pion.Click += pion_Click;
+            // 
+            // poziom
+            // 
+            poziom.Location = new Point(582, 361);
+            poziom.Name = "poziom";
+            poziom.Size = new Size(173, 52);
+            poziom.TabIndex = 4;
+            poziom.Text = "Odbicie w poziomie";
+            poziom.UseVisualStyleBackColor = true;
+            poziom.Click += poziom_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(poziom);
+            Controls.Add(pion);
+            Controls.Add(obrot_90);
             Controls.Add(Wczytaj);
             Controls.Add(pictureBox1);
             Name = "Form1";
@@ -69,5 +105,8 @@
 
         private PictureBox pictureBox1;
         private Button Wczytaj;
+        private Button obrot_90;
+        private Button pion;
+        private Button poziom;
     }
 }
--- obraz_wizualne/Form1.cs
+++ obraz_wizualne/Form1.cs
@@ -18,7 +18,7 @@ namespace obraz_wizualne
         {
             ofd.ShowDialog();
             string file_name = ofd.FileName;
-            pictureBox1.Image = Bitmap.FromFile (file_name);
+            pictureBox1.Image = Bitmap.FromFile(file_name);
             System.Drawing.Image image = pictureBox1.Image;
             pictureBox1.Image = image.GetThumbnailImage(pictureBox1.Width, pictureBox1.Height, null, IntPtr.Zero);
 
@@ -27,6 +27,45 @@ namespace obraz_wizualne
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void obrot_90_Click(object sender, EventArgs e)
+        {
+            Bitmap image = new Bitmap(pictureBox1.Image);
+
+            // Obrót obrazka o 90 stopni w prawo
+            image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+
+            // Aktualizacja PictureBox z obróconym obrazkiem
+            pictureBox1.Image = image;
+        }
+
+        private void pion_Click(object sender, EventArgs e)
+        {
+
+            // Wczytanie obrazka z pliku
+            Bitmap image = new Bitmap(pictureBox1.Image);
+
+            // Odbicie lustrzane obrazka w pionie
+            image.RotateFlip(RotateFlipType.RotateNoneFlipX);
+
+            // Aktualizacja PictureBox z odwróconym obrazkiem w pionie
+            pictureBox1.Image = image;
+
+        }
+
+        private void poziom_Click(object sender, EventArgs e)
+        { 
+
+            // Wczytanie obrazka z pliku
+            Bitmap image = new Bitmap(pictureBox1.Image);
+
+            // Odbicie lustrzane obrazka w poziomie
+            image.RotateFlip(RotateFlipType.RotateNoneFlipY);
+
+            // Aktualizacja PictureBox z odwróconym obrazkiem w poziomie
+            pictureBox1.Image = image;
 
         }
     }
